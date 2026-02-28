29a4a69af16c6f0a2778d52ea5bd8a6ec13b394e
halfdane <halfdane@gmx.net>
2026-02-28 12:58:06 +0100

    chore: bump version to v0.1.2

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
       in
       {
         packages.default =
-          let iliasVersion = "0.1.1"; in
+          let iliasVersion = "0.1.2"; in
           pkgs.buildGoModule {
             pname = "ilias";
             version = iliasVersion;
